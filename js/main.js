cd504071e6728626ef1914486c5363cb155be9c9
kingDaniel2004 <danielelvisr@gmail.com>
2022-05-21 10:40:38 -0400

    Co-authored-by: gorDaChris <gorDaChris@users.noreply.github.com> Co-authored-by: patelabhi2004 <patelabhi2004@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const themeMap = {
+    dark: "light",
+    light: "dark",
+    // solar: "dark"
+  };
+  
+  const theme = localStorage.getItem('theme')
+    || (tmp = Object.keys(themeMap)[0],
+        localStorage.setItem('theme', tmp),
+        tmp);
+  const bodyClass = document.body.classList;
+  bodyClass.add(theme);
+  
+  function toggleTheme() {
+    const current = localStorage.getItem('theme');
+    const next = themeMap[current];
+  
+    bodyClass.replace(current, next);
+    localStorage.setItem('theme', next);
+  }
+  
+  document.getElementById('themeButton').onclick = toggleTheme;
+  
+  
+  let sections = gsap.utils.toArray(".panel");
+  
+  console.log("hello world: " + sections)
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".container",
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      // base vertical scrolling on how wide the container is so it feels more natural.
+      end: "+=3500",
+    }
+  });
