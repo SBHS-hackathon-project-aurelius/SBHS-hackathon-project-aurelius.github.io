1fabd122bb91a417d37aa6ba2e1143ed50835a70
kingDaniel2004 <danielelvisr@gmail.com>
2022-05-21 13:12:15 -0400

    hello

--- js/login.js
+++ js/login.js
@@ -1,3 +1,6 @@
 function verifyLogin() {
-    var i = document.getElementById("")
+    document
+    var idData = document.getElementById("userId").value;
+    var blob = new Blob([idData], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "tempCredentials.txt");
 }
